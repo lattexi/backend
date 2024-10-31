5165391e9048b73ce318cbe4a1793cb00373c037
lattexi <laurikarhu@outlook.com>
2024-10-31 17:05:50 +0200

    Express assignment

--- readme.md
+++ readme.md
@@ -11,3 +11,22 @@
 - Didn't know what to do so made just simple min, max, sum, average and median calculator from the value keys in the list
 - Functionality: items can be posted on the server for example student exam numbers and then highest, lowest, average and median can be calculated
 - Works with /items/min, /items/max ect
+
+
+# Express
+
+## 1-2
+
+- Easy and straight forward
+
+## 3
+
+- Added information about api endpoints to index.pug
+- Little struggle with example data in pug
+- Used "each item in exampleData" for generating example data in pug
+
+# 4-5
+
+- Implemented PUT and DELETE endpoints
+- Added images to media folder with matching filenames to the mock data
+- Images avalaible at /media/filename
--- src/index.js
+++ src/index.js
@@ -1,40 +1,53 @@
-import http from 'http';
-import { getItems, postItem, deleteItem, changeItem, changeItemProperty, getMin, getMax, getSum, getAverage, getMedian } from './items.js';
+import express from 'express';
+import { getItemById, getItems, mediaItems, changeItem, postItem, deleteItem } from './media.js';
 const hostname = '127.0.0.1';
 const port = 3000;
+const app = express();
 
-// Create a server object and bind a callback function
-// to all request events
-const server = http.createServer((req, res) => {
-    const { url, method } = req;
-    console.log('url:', url, 'method:', method);
-    if (url === '/items' && method === 'GET') {
-        getItems(res);
-    } else if (url === '/items' && method === 'POST') {
-        postItem(req, res);
-    } else if (url === '/items' && method === 'DELETE') {
-        deleteItem(req, res);
-    } else if (url === '/items' && method === 'PUT') {
-        changeItem(req, res);
-    } else if (url === '/items' && method === 'PATCH') {
-        changeItemProperty(req, res);
-    } else if (url === '/items/min' && method === 'GET') {
-        getMin(res);
-    } else if (url === '/items/max' && method === 'GET') {
-        getMax(res);
-    } else if (url === '/items/sum' && method === 'GET') {
-        getSum(res);
-    } else if (url === '/items/average' && method === 'GET') {
-        getAverage(res);
-    } else if (url === '/items/median' && method === 'GET') {
-        getMedian(res);
-    } else {
-        // Generic not found response
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ error: '404', message: 'not found' }));
-    }
+app.set('view engine', 'pug');
+app.set('views', 'src/views');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Welcome', message: 'Welcome to the API!' });
 });
 
-server.listen(port, hostname, () => {
+// Home page (client) as static html, css, js
+app.use(express.static('public'));
+// Uploaded media files
+app.use('/media', express.static('media'));
+
+// Api documentation tms. with pug
+app.get('/api', (req, res) => {
+    res.render('index', {
+        title: 'API Documentation',
+        message: 'Welcome to my API!',
+        exampleData: mediaItems,
+    });
+});
+
+// Media resource endpoints
+app.get('/api/media', (req, res) => {
+    getItems(res);
+});
+
+app.get('/api/media/:id', (req, res) => {
+    getItemById(req, res);
+});
+
+app.post('/api/media', (req, res) => {
+    postItem(req, res);
+});
+
+app.put('/api/media/:id', (req, res) => {
+    changeItem(req, res);
+});
+
+app.delete('/api/media/:id', (req, res) => {
+    deleteItem(req, res);
+});
+
+app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
